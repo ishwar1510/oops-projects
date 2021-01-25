61996c5ce414090cd176be9b12faf63d088039ca
ishwar1510 <ijh.hkr9@gmail>
2021-01-25 06:44:43 +0530

    class in oops

---
+++ oopsclass.js
@@ -0,0 +1,42 @@
+// class UnionBankOfIndia{
+//     customer_Name = "Ishwar";
+//     acc_No = 123456;
+//     loc = "kerala";
+//     details = () => {
+//         // console.log(
+//             // "customer name:", this.customer_Name,
+//             // "customer acc num:", this.acc_No);
+//     }
+// }
+// let x = new UnionBankOfIndia();
+// console.log(x);
+
+
+// class BankUser {
+//     acc_No = 4781;
+//     acc_Name = "Narendra modi";
+//     getDetails = () => {
+//         console.log(
+//             "My Account No:",
+//             this.acc_No,
+//             " and My A/c Name:",
+//             this.acc_Name
+//         );
+//     };
+// }
+//
+// let newUser = new BankUser();
+//
+// console.log(newUser);
+
+
+
+class Customer {
+    acc_No = 7481;
+}
+
+console.log( new Customer());  //one type of execution
+console.log(new Customer().acc_No); //2nd type of execution
+//
+// let cust = new customer();
+// console.log(cust);
